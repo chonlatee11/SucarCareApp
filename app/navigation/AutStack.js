7658d8c259817e9e545ea568f56099b5753622e3
Chonlatee Khamlue <46771457+chonlatee11@users.noreply.github.com>
2023-03-06 21:46:35 +0700

    3/6/2023

--- app/navigation/AutStack.js
+++ app/navigation/AutStack.js
@@ -3,7 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import SignInScreen from '../screens/login/SignInScreen';
 import SignUpScreen from '../screens/register/SignUpScreen';
-import AddressInput from '../screens/register/AddressInput';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +12,6 @@ const AuthStack = () => {
         <Stack.Navigator screenOptions={{headerShown: true}}>
             <Stack.Screen name='SignIn' component={SignInScreen} />
             <Stack.Screen name='SignUp' component={SignUpScreen} /> 
-            <Stack.Screen name='AddressInput' component={AddressInput} /> 
         </Stack.Navigator>
     );
   };
